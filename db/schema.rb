747cf9bf650e8c10d59ed0b06bb3674729a7f28d
ryan <ryanwkoch@gmail.com>
2017-08-28 12:53:46 -0400

    added foreign key to routine table
    rails generate migration AddExerciseRefToRoutine exercise: references
    https://github.com/ga-wdi-boston/capstone-project/issues/812

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828164832) do
+ActiveRecord::Schema.define(version: 20170828165203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,8 +33,10 @@ ActiveRecord::Schema.define(version: 20170828164832) do
   create_table "routines", force: :cascade do |t|
     t.string   "name"
     t.date     "date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "exercise_id"
+    t.index ["exercise_id"], name: "index_routines_on_exercise_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,4 +50,5 @@ ActiveRecord::Schema.define(version: 20170828164832) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "routines", "exercises"
 end
