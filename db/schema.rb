5947ae67aa1a7fc69e4fd20b2603babe2f10c309
ryan <ryanwkoch@gmail.com>
2017-08-28 14:58:14 -0400

    removed date from routine schema
    date was a bit redundant, I'd prefer to use updated_at or created_at
    as the date on the front end

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828165449) do
+ActiveRecord::Schema.define(version: 20170828185702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 20170828165449) do
 
   create_table "routines", force: :cascade do |t|
     t.string   "name"
-    t.date     "date"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "exercise_id"
