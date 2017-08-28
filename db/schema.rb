bb4b805c6733832d5915a86b6c2789d6fdebe76f
ryan <ryanwkoch@gmail.com>
2017-08-28 12:55:23 -0400

    added user foreign key to routines
    rails generate migration AddUserRefToRoutine user:references
    https://github.com/ga-wdi-boston/capstone-project/issues/812
    creating join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828165203) do
+ActiveRecord::Schema.define(version: 20170828165449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,9 @@ ActiveRecord::Schema.define(version: 20170828165203) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "exercise_id"
+    t.integer  "user_id"
     t.index ["exercise_id"], name: "index_routines_on_exercise_id", using: :btree
+    t.index ["user_id"], name: "index_routines_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,4 +53,5 @@ ActiveRecord::Schema.define(version: 20170828165203) do
 
   add_foreign_key "examples", "users"
   add_foreign_key "routines", "exercises"
+  add_foreign_key "routines", "users"
 end
